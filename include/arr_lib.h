ba1ca06423ac93f0335e5705dd80419cadf555a8
Dmitriy <dionyaaa1554@gmail.com>
2026-06-11 17:44:18 +0300

    fix: add missing <stddef.h> to arr_lib.h for size_t type

--- include/arr_lib.h
+++ include/arr_lib.h
@@ -1,6 +1,8 @@
 #ifndef ARR_LIB_H
 #define ARR_LIB_H
 
+#include <stddef.h>
+
 void* arr_extremum(const void* array, size_t array_size, size_t block_size, int (*cmp)(const void* a, const void* b));
 
 #endif
