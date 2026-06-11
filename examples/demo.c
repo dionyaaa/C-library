cd95c179054e300aaa0eed16f488f4f20dcf3d01
Dmitriy <dionyaaa1554@gmail.com>
2026-06-11 17:51:39 +0300

    feat: update demo.c with arr_extremum and arr_print functions

--- examples/demo.c
+++ examples/demo.c
@@ -1,7 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_NONSTDC_NO_DEPRECATE
-
 #define ARRAY_SIZE 16
+#define NUMBERS 5
 
 #include <stdio.h>
 #include <stdint.h>
@@ -9,24 +7,29 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <time.h>
+
 #include "../include/io_lib.h"
 #include "../include/sort_lib.h"
 #include "../include/cmp_lib.h"
 #include "../include/rand_lib.h"
+#include "../include/arr_lib.h"
+#include "../include/utils_lib.h"
 
 int main(void)
 {
 	int selected_library;
 	int symbol;
-	srand(time(NULL));
+
+	srand((unsigned int)time(NULL));
 
 	puts("Select a module to test:");
 	puts("1. Input/Output module;");
 	puts("2. Sorting module;");
 	puts("3. Random number generator module;");
+	puts("4. Exit;");
 	putchar('\n');
 
-	puts("Enter your choice (1-3):");
+	puts("Enter your choice (1-4):");
 
 	if (scanf("%d", &selected_library) < 1)
 	{
@@ -42,79 +45,117 @@ int main(void)
 	putchar('\n');
 
 	switch (selected_library) {
-	case 1: {
-		puts("Enter a string of any length:");
-		char* input_string = getstr();
-		if (input_string == NULL)
-		{
-			puts("Memory allocation error!");
-			return 2;
+		case 1: {
+			puts("Enter a string of any length:");
+			char* input_string = getstr();
+			if (input_string == NULL)
+			{
+				puts("Memory allocation error!");
+				return 2;
+			}
+			printf("You entered the string:\n\"%s\"\n", input_string);
+			free(input_string);
+			break;
 		}
-		printf("You entered the string:\n\"%s\"\n", input_string);
-		free(input_string);
-		break;
-	}
-	case 2: {
-		int first_array[ARRAY_SIZE];
-		int second_array[ARRAY_SIZE];
-		int third_array[ARRAY_SIZE];
-		int fourth_array[ARRAY_SIZE];
-		int* arrays[4] = {first_array, second_array, third_array, fourth_array};
+		case 2: {
+			int first_array[ARRAY_SIZE];
+			int second_array[ARRAY_SIZE];
+			int third_array[ARRAY_SIZE];
+			int fourth_array[ARRAY_SIZE];
+			int* arrays[4] = {first_array, second_array, third_array, fourth_array};
 
-		puts("Generating random arrays...");
-		for (int i = 0; i < ARRAY_SIZE; i++)
-		{
-			first_array[i] = (int)rand_32(101) - 50;
-			second_array[i] = (int)rand_32(101) - 50;
-			third_array[i] = (int)rand_32(101) - 50;
-			fourth_array[i] = (int)rand_32(101) - 50;
-		}
-		putchar('\n');
-
-		puts("Generated arrays:");
-		for (int i = 0; i < 4; i++)
-		{
-			printf("Array #%d: ", i + 1);
-			for (int j = 0;j < ARRAY_SIZE;j++)
-				printf("%d ", arrays[i][j]);
+			puts("Generating random arrays...");
+			for (int i = 0; i < ARRAY_SIZE; i++)
+			{
+				first_array[i] = (int)rand_32(51) - 25;
+				second_array[i] = (int)rand_32(51) - 25;
+				third_array[i] = (int)rand_32(51) - 25;
+				fourth_array[i] = (int)rand_32(51) - 25;
+			}
 			putchar('\n');
-		}
-		putchar('\n');
 
-		puts("Sorting arrays...");
-		if (!insertion_sort(first_array, ARRAY_SIZE, sizeof(int), cmp_int_asc))
-			return 3;
-		if (!insertion_sort(second_array, ARRAY_SIZE, sizeof(int), cmp_int_desc))
-			return 3;
-		if (!counting_sort(third_array, ARRAY_SIZE, true))
-			return 3;
-		if (!counting_sort(fourth_array, ARRAY_SIZE, false))
-			return 3;
-		putchar('\n');
-
-		puts("Sorted arrays:");
-		puts("1/2 - Insertion sort (Ascending and descending)");
-		puts("3/4 - Counting sort (Ascending and descending)");
-		putchar('\n');
-
-		for (int i = 0; i < 4; i++)
-		{
-			printf("Sorted array #%d: ", i + 1);
-			for (int j = 0;j < ARRAY_SIZE;j++)
-				printf("%d ", arrays[i][j]);
+			puts("Generated arrays:");
+			for (int i = 0; i < 4; i++)
+			{
+				printf("Array #%d: ", i + 1);
+				if (!arr_print(arrays[i], ARRAY_SIZE, sizeof(int), print_int, " "))
+				{
+					puts("Function arr_print() argument error!");
+					return 3;
+				}
+				putchar('\n');
+			}
 			putchar('\n');
+			
+			puts("Maximum and minimum of arrays:");
+			for (int i = 0; i < 4; i++)
+			{
+				int* maximum = (int*)arr_extremum(arrays[i], ARRAY_SIZE, sizeof(int), cmp_int_asc);
+				int* minimum = (int*)arr_extremum(arrays[i], ARRAY_SIZE, sizeof(int), cmp_int_desc);
+				if (maximum == NULL || minimum == NULL)
+				{
+					puts("Function arr_extremum() argument error!");
+					return 3;
+				}
+				printf("Array #%d: maximum = %d, minimum = %d", i + 1, *maximum, *minimum);
+				putchar('\n');
+			}
+			putchar('\n');
+
+			puts("Sorting arrays...");
+			if (!insertion_sort(first_array, ARRAY_SIZE, sizeof(int), cmp_int_asc))
+			{
+				puts("Function insertion_sort() some error!");
+				return 4;
+			}
+			if (!insertion_sort(second_array, ARRAY_SIZE, sizeof(int), cmp_int_desc))
+			{
+				puts("Function insertion_sort() some error!");
+				return 4;
+			}
+			if (!counting_sort(third_array, ARRAY_SIZE, true))
+			{
+				puts("Function counting_sort() some error!");
+				return 4;
+			}
+			if (!counting_sort(fourth_array, ARRAY_SIZE, false))
+			{
+				puts("Function counting_sort() some error!");
+				return 4;
+			}
+			putchar('\n');
+
+			puts("Sorted arrays:");
+			puts("1/2 - Insertion sort (Ascending and descending)");
+			puts("3/4 - Counting sort (Ascending and descending)");
+			putchar('\n');
+
+			for (int i = 0; i < 4; i++)
+			{
+				printf("Sorted array #%d: ", i + 1);
+				if (!arr_print(arrays[i], ARRAY_SIZE, sizeof(int), print_int, " "))
+				{
+					puts("Function arr_print() argument error!");
+					return 3;
+				}
+				putchar('\n');
+			}
+			break;
 		}
-		break;
-	}
-	case 3: {
-		puts("Random number in range [-2^30, 2^30 - 1]:");
-		printf("%d\n", (int)rand_32((uint32_t)INT_MAX + 1) - 1073741824);
-		break;
-	}
-	default: {
-		puts("Invalid input!");
-		return 1;
-	}
+		case 3: {
+			puts("Random numbers in range [-2^30, 2^30 - 1]:");
+			for (size_t i = 0; i < NUMBERS; i++)
+				printf("%d\n", (int)rand_32((uint32_t)INT_MAX + 1) - 1073741824);
+			break;
+		}
+		case 4: {
+			puts("Exit...");
+			break;
+		}
+		default: {
+			puts("Invalid input!");
+			return 1;
+		}
 	}
 	return 0;
 }
