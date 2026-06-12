68cfb86101ee0fb7f4ab3e3692d95dd1eb03c5c9
Dmitriy <dionyaaa1554@gmail.com>
2026-06-12 17:14:24 +0300

    feat: add hybrid val_swap function with stack/heap optimization

--- include/utils_lib.h
+++ include/utils_lib.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+#define MAX_STACK_SIZE_FOR_SWAP 64
+
+bool val_swap_stack(void* a, void* b, size_t size);
+bool val_swap_heap(void* a, void* b, size_t size);
 bool val_swap(void* a, void* b, size_t size);
 
 #endif
--- source/utils_lib.c
+++ source/utils_lib.c
@@ -1,13 +1,28 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include <string.h>
 #include "../include/utils_lib.h"
 
-bool val_swap(void* a, void* b, size_t size)
+
+bool val_swap_stack(void* a, void* b, size_t size)
 {
-	if (a == NULL || b == NULL || size == 0)
-		return false;
+	assert(a != NULL && b != NULL && size != 0 && size <= MAX_STACK_SIZE_FOR_SWAP);
+
+	unsigned char tmp[MAX_STACK_SIZE_FOR_SWAP];
+
+	memcpy(tmp, a, size);
+	memcpy(a, b, size);
+	memcpy(b, tmp, size);
+
+	return true;
+}
+
+
+bool val_swap_heap(void* a, void* b, size_t size)
+{
+	assert(a != NULL && b != NULL && size != 0);
 
 	void* tmp = malloc(size);
 	if (tmp == NULL)
@@ -19,4 +34,15 @@ bool val_swap(void* a, void* b, size_t size)
 
 	free(tmp);
 	return true;
+}
+
+
+bool val_swap(void* a, void* b, size_t size)
+{
+	assert(a != NULL && b != NULL && size != 0);
+
+	if (size <= MAX_STACK_SIZE_FOR_SWAP)
+		return val_swap_stack(a, b, size);
+	else
+		return val_swap_heap(a, b, size);
 }
